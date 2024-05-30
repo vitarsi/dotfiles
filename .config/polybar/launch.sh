07b3d09758209ed3968be014cf47414cebc38d69
vitarsi <vitarsi@users.noreply.github.com>
2024-05-30 18:35:58 +0530

    Added clock

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar -c=~/.config/polybar/config.ini top 2>&1 | tee -a /tmp/polybar.log & disown
-polybar -c=~/.config/polybar/config.ini bottom 2>&1 | tee -a /tmp/polybar.log & disown
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar topdummy 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bottom 2>&1 | tee -a /tmp/polybar2.log & disown
+# polybar topside 2>&1 | tee -a /tmp/polybar1.log & disown
+
+polybar clock 2>&1 | tee -a /tmp/polybar1.log & disown
+echo "Bars launched..."
